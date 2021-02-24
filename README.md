f3af2746ad98eb2a01a05459291a3d0c44362ac3
kor-taeyoon <dbsl0512@gmail.com>
2021-02-24 17:25:33 +0900

    Completed logic. except real flight test :)

---
+++ README.md
@@ -0,0 +1,25 @@
+## 6 Channel Transmitter
+- - -
+
+### Summary
+> 
+>
+> 참빛설계학기 프로젝트를 진행하면서 개발하는 무인기 제어용 송신기 입니다.
+
+
+### Parts
+>
+> - Arduino Nano (328p, old bootloader(57600))
+> 
+> - nRF24L01 + PA + LNA
+> 
+> - 2 * Joystick 
+
+
+
+
+### References
+
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,121 @@
-#include <Arduino.h>
 
+// PreFix
+// ========== ========== ========== ========== 
+// nRF24L01 Module
+#define pin_CE  8
+#define pin_CSN 10
+
+// Potentiometer
+#define pin_trans_t A0  // potentiometer
+
+// Joystick
+//#define pin_pr_sw 2
+//#define pin_ty_sw 3
+//#define pin_trans_t A4  // joystick
+#define pin_trans_r A3
+#define pin_trans_e A2
+#define pin_trans_a A1
+
+// Button
+#define pin_btn_dji 4
+#define pin_btn_diy 5
+
+// Buzzer
+#define pin_buzzer 9
+
+// LED
+#define pin_led_dji 7
+#define pin_led_diy 6
+
+
+
+// Libraries
+// ========== ========== ========== ========== 
+#include <Arduino.h>
+#include <SimpleTimer.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+#include <SPI.h>
+
+
+
+// Objects & Variables
+// ========== ========== ========== ========== 
+RF24 radio(pin_CE, pin_CSN); // CE, CSN
+const byte address[6] = "01001";
+SimpleTimer timer;
+Data_to_be_sent sent_data;
+
+int diy_toggle = 0;
+int diy_last_pressed = 0;
+int dji_toggle = 0;
+int dji_last_pressed = 0;
+
+
+
+// Struct, Functions
+// ========== ========== ========== ========== 
+struct Data_to_be_sent {
+    byte T;
+    byte R;
+    byte E;
+    byte A;
+    byte M;
+    byte X;
+};
+
+void BtnChecker(){
+    if(digitalRead(pin_btn_dji) == 0){
+        digitalWrite(pin_led_dji, !digitalRead(pin_led_dji));
+    }
+    if(digitalRead(pin_btn_diy) == 0){
+        digitalWrite(pin_led_diy, !digitalRead(pin_led_diy));
+    }
+}
+
+
+
+// Main Flow
+// ========== ========== ========== ========== 
 void setup() {
-  // put your setup code here, to run once:
+    // nRF tx setting
+    radio.begin();
+    radio.setAutoAck(false);
+    radio.setDataRate(RF24_250KBPS);
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MAX);
+    
+    // data packet initialize
+    sent_data.T = 127;
+    sent_data.R = 127;
+    sent_data.E = 127;
+    sent_data.A = 127;
+    sent_data.M = 0;
+    sent_data.X = 0;
+
+    // pin Initializing
+    pinMode(pin_btn_diy, INPUT_PULLUP);
+    pinMode(pin_btn_dji, INPUT_PULLUP);
+    pinMode(pin_buzzer, OUTPUT);
+    pinMode(pin_led_diy, OUTPUT);
+    pinMode(pin_led_dji, OUTPUT);
+
+    // timer
+    timer.setInterval(500, BtnChecker);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // timer loop
+    timer.run();
+    
+    // data refresh
+    sent_data.T = map(analogRead(pin_trans_t), 0, 1023, 0, 255);
+    sent_data.R = map(analogRead(pin_trans_r), 0, 1023, 0, 255);
+    sent_data.E = map(analogRead(pin_trans_e), 0, 1023, 0, 255);
+    sent_data.A = map(analogRead(pin_trans_a), 0, 1023, 0, 255);
+    sent_data.M = digitalRead(pin_btn_dji);
+    sent_data.X = digitalRead(pin_btn_diy);
+
+    // signal transmit
+    radio.write(&sent_data, sizeof(sent_data));
 }
